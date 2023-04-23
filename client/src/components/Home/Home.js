c6c91936a8d7f4642b41955f3c0d04668654c4f4
Esoteriker <hai_designer@hotmail.com>
2023-04-23 22:58:30 +0200

    search; add search line;

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,31 +1,114 @@
 import React from "react";
-import { Container, Grow, Grid } from "@mui/material";
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button, Autocomplete, Chip } from "@mui/material";
 
+import { MuiChipsInput } from 'mui-chips-input';
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 import { useDispatch } from 'react-redux';
-import { getPosts } from '../../actions/posts';
-import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
+import { getPosts, getPostsBySearch } from '../../actions/posts';
+import { useState, useEffect, useRef } from 'react';
+import Pagination from '../Pagination';
+
+import useStyles from "./styles";
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
 
     const [currentId, setCurrentId] = useState(null);
     const dispatch = useDispatch();
+    const query = useQuery();
+    const navigate = useNavigate();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const classes = useStyles();
+    const [search, setSearch] = useState(''); //[europe, asia] => europe, asia
+    const [tags, setTags] = useState([]);
+    const chipsInputRef = useRef(null);
 
     useEffect(() => {
         dispatch(getPosts());
     }, [currentId, dispatch]);
 
+    const searchPost = () => {
+        if (search.trim() || tags) {
+            dispatch(getPostsBySearch({ search, tags: tags.join(',') }));
+            navigate(`/posts/search?searchQuery=${search || 'none'}&tags=${tags.join(',')}`);
+        } else {
+            navigate('/');
+        }
+    };
+
+    const handlekeyPress = (e) => {
+        if (e.keyCode === 13) {
+            searchPost();
+        }
+    };
+
+    const handleAddChip = (tag) =>  {
+        console.log(tag)
+        setTags([...tags, tag]);
+    };
+
+    const handleDeleteChip = (tagToDelete) => {
+        console.log(tagToDelete)
+        setTags(tags.filter((tag) => tag !== tagToDelete));
+    };
+
+    const handleKeyPressTags = (e) => {
+        if (e.key === 'Enter' && e.target.value !== '') {
+            handleAddChip(e.target.value);
+            e.target.value = '';
+          }
+      };
+
     return (
         <Grow in>
-        <Container>
-            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                <Grid item xs={12} sm={7}>
+        <Container maxWidth="xl">
+            <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+                <Grid item xs={12} sm={6} md={9}>
                     <Posts setCurrentId={setCurrentId} />
                 </Grid>   
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={6} md={3}>
+                <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                    <TextField 
+                        name="search"
+                        variant="outlined"
+                        label="Search Memories"
+                        onKeyPress={handlekeyPress}
+                        fullWidth
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        style={{ marginTop: 15 }}
+                    />
+                     <div>
+                        <TextField
+                            label="Add a tag"
+                            variant="outlined"
+                            onKeyPress={handleKeyPressTags}
+                            fullWidth
+                            inputProps={{ placeholder: 'Type and press enter' }}
+                            style={{ marginTop: 15 }}
+                        />
+                        {tags.map((tag) => (
+                            <Chip
+                            key={tag}
+                            label={tag}
+                            onDelete={() => handleDeleteChip(tag)}
+                            style={{ margin: 5 }}
+                            />
+                        ))}
+                    </div>
+                    <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+                </AppBar>
                     <Form currentId={currentId} setCurrentId={setCurrentId} />
+                    <Paper elevation={6}>
+                        <Pagination />
+                    </Paper>
                 </Grid> 
             </Grid>     
         </Container>
